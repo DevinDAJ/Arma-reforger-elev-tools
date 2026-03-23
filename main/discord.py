ae78c318d79d8f4aca77bc2c138157875ce4fcf8
Dave Pradana <devin.pradana.r@gmail.com>
2026-03-23 08:57:19 +0700

    added some sample code

--- main/discord.py
+++
@@ -1,3 +0,0 @@
-
-PUBLIC_KEY          = os.environ["DISCORD_PUBLIC_KEY"]
-DISCORD_API_BASE    = "https://discord.com/api/v10"
--- main/elev_calculator_discord_bot.py
+++ main/elev_calculator_discord_bot.py
@@ -1,3 +1,13 @@
 
+PUBLIC_KEY          = os.environ["DISCORD_PUBLIC_KEY"]
+DISCORD_API_BASE    = "https://discord.com/api/v10"
 
+def verify_key(body: bytes, sig: str, timestamp: str, public_key: str) -> bool:
+    """Return True if the Ed25519 signature is valid for the given body."""
+    try:
+        verify_key = nacl.signing.VerifyKey(bytes.fromhex(public_key))
+        verify_key.verify(f"{timestamp}".encode() + body, bytes.fromhex(sig))
+        return True
+    except (nacl.exceptions.BadSignatureError, ValueError):
+        return False
 
